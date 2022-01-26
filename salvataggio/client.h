de7d999dbce346cfda9d046a28daca21c814ab7c
Luck199 <92259877+Luck199@users.noreply.github.com>
2022-01-26 22:57:53 +0100

    Delete salvataggio directory

--- salvataggio/client.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLIENT_H_
-#define CLIENT_H_
-
-
-
-//void crea_connessione(struct sockaddr_un *psa);
-#endif
-
-
--- salvataggio/worker.h
+++
@@ -1,7 +0,0 @@
-#ifndef WORKER_H_
-#define WORKER_H_
-
-void inizializza_worker();
-void* vitaWorker(void*  idWorker);
-
-#endif
